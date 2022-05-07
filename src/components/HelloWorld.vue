5d2aeb80d506d351d3dfa94ac4d275c8ea368f0d
Jostein Skaar <jostein.skaar@gmail.com>
2022-05-07 19:52:34 +0200

    Type application

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,7 +9,8 @@ const count = ref(0);
 <template>
   <h1>{{ msg }}</h1>
 
-  <p>Forsøk 5: <a href="/pakke3.mcpack" target="_self">Last ned pakke blank</a></p>
+  <p>Forsøk 6: <a href="/pakke3.mcpack" download="pakka4.mcpack" type="application">Last ned pakke blank</a></p>
+  <p>Forsøk 7: <a href="/pakke3.mcpack" target="_self" type="application">Last ned pakke blank</a></p>
 </template>
 
 <style scoped></style>
