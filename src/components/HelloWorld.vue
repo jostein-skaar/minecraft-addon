d20548630df2486f9cb7ccc218de6984d668d5e0
Jostein Skaar <jostein.skaar@gmail.com>
2022-05-07 19:21:13 +0200

    Trying _self

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,8 +9,7 @@ const count = ref(0);
 <template>
   <h1>{{ msg }}</h1>
 
-  <p>Forsøk 3: <a href="/pakke3.mcpack" target="_blank">Last ned pakke blank</a></p>
-  <p>Forsøk 4: <a href="/pakke3.mcpack" target="_blank" type="application/zip">Last ned pakke blank + type zip</a></p>
+  <p>Forsøk 5: <a href="/pakke3.mcpack" target="_self">Last ned pakke blank</a></p>
 </template>
 
 <style scoped></style>
